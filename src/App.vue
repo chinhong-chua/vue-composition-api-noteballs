2bd3f5feb89895bc3d4b6107be238512e4d6f6e3
Cruz <cruz_ch96@outlook.com>
2022-08-25 00:17:07 +0800

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+<NavBar/>
+<!-- <RouterLink to="/" class="button">Notes</RouterLink>|
+<RouterLink to="/stats" class="button">Stats</RouterLink> -->
+<div class="container is-max-desktop">
+<RouterView />
+
+</div>
+</template>
+
+<script setup>
+import NavBar from './components/Layout/NavBar.vue';
+
+
+</script>
+
+<style>
+@import 'bulma/css/bulma.min.css';
+</style>
---
+++ src/views/ViewNotes.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="notes">
+    <div class="card has-background-success-dark p-4 mb-5">
+      <div class="field">
+        <div class="control">
+          <textarea class="textarea" placeholder="Add a new note" />
+        </div>
+      </div>
+
+      <div class="field is-grouped is-grouped-right">
+        <div class="control">
+          <button class="button is-link has-background-success">
+            Add New Note
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <div class="card mt-4" v-for="i in 3">
+      <div class="card-content">
+        <div class="content">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
+        </div>
+      </div>
+      <footer class="card-footer">
+        <a href="#" class="card-footer-item">Edit</a>
+        <a href="#" class="card-footer-item">Delete</a>
+      </footer>
+    </div>
+  </div>
+</template>
